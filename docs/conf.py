e3094b4cfbc9e95884190c416d3f3e2eb443e104
faffyswap <faffyswap@gmail.com>
2023-07-07 18:26:51 +0200

    try sphinx.ext.githubpages to iix GH README

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,12 @@ import terra_algo_backtest  # noqa: E402
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "nbsphinx"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "nbsphinx",
+    "sphinx.ext.githubpages",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
